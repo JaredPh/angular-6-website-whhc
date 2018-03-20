13eba53c1eb7593f1a61bbc476d1e42ea6ebbb85
Jared Phayer <mail@jared.ph>
2018-03-20 09:18:50 +0000

    initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'whhc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'whhc';
+}
