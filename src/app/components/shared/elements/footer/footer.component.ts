167cc7e2d9a955c66a6e1641a4abe5589ab24281
Jared Phayer <mail@jared.ph>
2018-05-07 01:02:29 +0100

    Footer - Added sponsors

--- src/app/components/shared/elements/footer/footer.component.ts
+++ src/app/components/shared/elements/footer/footer.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {IImage} from "../../../media/media.interfaces";
+import { IImage } from '../../../media/media.interfaces';
 
-// todo: move me
-export interface ISocialNetwork {
- id: string;
- label: string;
- logo: IImage;
- url: string;
+interface ILink {
+  id: string;
+  label: string;
+  logo: IImage;
+  url: string;
 }
 
 @Component({
@@ -16,7 +15,40 @@ export interface ISocialNetwork {
 })
 export class FooterComponent implements OnInit {
 
-  public socialNetworks: ISocialNetwork[] = [
+  public links: ILink[] = [
+    {
+      id: 'studio-society',
+      label: 'Studio Society',
+      logo: { url: '/assets/site-studio.jpg', width: 200, height: 200 },
+      url: 'https://studio-society.com/',
+    },
+    {
+      id: 'alan-day',
+      label: 'Alan Day VW',
+      logo: { url: '/assets/alan-day-vw.png', width: 200, height: 200 },
+      url: 'https://www.alandayvw.co.uk/',
+    },
+    {
+      id: 'one-bourbon-tavern',
+      label: 'One Bourbon Tavern',
+      logo: { url: '/assets/one-bourbon-tavern.jpg', width: 200, height: 200 },
+      url: 'http://www.onebourbon.co.uk/',
+    },
+    {
+      id: 'cumberland-tennis',
+      label: 'Cumberland Lawn Tennis Club',
+      logo: { url: '/assets/cltc.png', width: 200, height: 200 },
+      url: 'https://www.cltc-hcc.com/',
+    },
+    {
+      id: 'hampstead-cricket',
+      label: 'Hampstead Cricket Club',
+      logo: { url: '/assets/hcc.png', width: 200, height: 200 },
+      url: 'https://www.cltc-hcc.com/cricket/',
+    },
+  ];
+
+  public socialNetworks: ILink[] = [
     {
       id: 'instagram',
       label: '/westhampsteadhockeyclub',
