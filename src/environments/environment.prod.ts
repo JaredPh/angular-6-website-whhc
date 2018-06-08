6773c9806ab1364e693ce9a6fdf8a2aa007fdb39
Jared Phayer <mail@jared.ph>
2018-06-08 20:38:30 +0100

    Adhoc - added api param to http service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiAddress: 'https://api.whhc.uk',
+  mapApiKey: 'AIzaSyB5yV1QqO2at1823hqRNlS9uBlv15LyqIQ',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiAddress: 'http://localhost:3000',
+  mapApiKey: 'AIzaSyB5yV1QqO2at1823hqRNlS9uBlv15LyqIQ',
 };
